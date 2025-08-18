42e75f42400df904c55cd1066d6c2d9c5a94625f
Conor Laver <conorlaver@gmail.com>
2025-08-18 12:24:06 -0400

    move tests

--- tests/test-widget-server.py
+++ tests/test-widget-server.py
@@ -2,23 +2,25 @@
 """Simple HTTP server to test the widget locally"""
 
 import http.server
-import socketserver
 import os
+import socketserver
 
 PORT = 8888
 DIRECTORY = "."
+
 
 class CORSRequestHandler(http.server.SimpleHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, directory=DIRECTORY, **kwargs)
 
     def end_headers(self):
-        self.send_header('Access-Control-Allow-Origin', '*')
-        self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
-        self.send_header('Access-Control-Allow-Headers', 'Content-Type')
+        self.send_header("Access-Control-Allow-Origin", "*")
+        self.send_header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+        self.send_header("Access-Control-Allow-Headers", "Content-Type")
         super().end_headers()
 
-os.chdir('/Users/conor/repos/whatsupdoc')
+
+os.chdir("/Users/conor/repos/whatsupdoc")
 with socketserver.TCPServer(("", PORT), CORSRequestHandler) as httpd:
     print(f"Server running at http://localhost:{PORT}/")
     print(f"Open http://localhost:{PORT}/test-widget-local.html to test the widget")
